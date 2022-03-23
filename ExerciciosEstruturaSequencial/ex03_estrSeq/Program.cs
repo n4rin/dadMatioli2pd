e8c6a0be54f11b20312bd66c68c37e2801cff53f
vxpresto <nallinguilherme@gmail.com>
2022-03-22 23:46:48 -0300

    ExEstrutSequenEx03

---
+++ ExerciciosEstruturaSequencial/ex03_estrSeq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex03_EstrSeq // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, diferenca;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+
+            diferenca = (a*b) - (c*d);
+
+            Console.WriteLine("DIFERENÇA = " + diferenca);
+        }
+    }
+}
