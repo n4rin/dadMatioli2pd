817a328d0bb1d7e824eecb5e2c93c75d2acbd171
vxpresto <nallinguilherme@gmail.com>
2022-03-22 23:39:15 -0300

    exEstruSequenEx01

---
+++ ExerciciosEstruturaSequencial/ex01_estrSeq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex01_estrSeq // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, soma;
+
+            n1 = Int32.Parse(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            soma = n1 + n2;
+
+            Console.WriteLine("A SOMA É: " + soma);
+        }
+    }
+}
