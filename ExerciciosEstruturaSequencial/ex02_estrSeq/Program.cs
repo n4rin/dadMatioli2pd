8212b279ad4e7895e0e1a12a13de4c12438d379a
vxpresto <nallinguilherme@gmail.com>
2022-03-22 23:43:01 -0300

    exEstrutSequen02

---
+++ ExerciciosEstruturaSequencial/ex02_estrSeq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex02_estrSeq // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area;
+
+            raio = Convert.ToInt32(Console.ReadLine());
+            area = 3.141519*(raio*raio);
+
+            Console.WriteLine("A área do círculo é: {0:0.0000}", area);
+        }
+    }
+}
